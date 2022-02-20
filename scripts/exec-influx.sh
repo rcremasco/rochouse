28527b68001366b9fbd0e4f41901404498b4bb30
rcremasco <roberto.cremasco@gmail.com>
2022-02-20 21:12:28 +0100

    dev

--- scripts/exec-influx.sh
+++ scripts/exec-influx.sh
@@ -128,40 +128,88 @@ restoreDB()
 
 }
 
+backupDB()
+{
+
+  echo "deleting old backup"
+  rm /backup/influx.bck/ha/*
+  rm /backup/influx.bck/telegraf/*
+
+  echo "backup ha db"
+  docker exec -i influxdb influxd  backup -portable -database ha -host 127.0.0.1:8088 /backup/influx.bck/ha
+  echo "done"
+
+  echo "backup telegraf db"
+  docker exec -i influxdb influxd  backup -portable -database telegraf -host 127.0.0.1:8088 /backup/influx.bck/telegraf
+  echo "done"
+
+}
+
+saveImage()
+{
+  if [ ! -d /backup/images/influxdb ]; then
+    echo "creating backup folder"
+    sudo mkdir -p /backup/images/influxdb/
+    sudo chown pi:pi /backup/images/influxdb/
+  fi
+  echo "starting docker image save"
+  docker save --output /backup/images/influxdb/influxdb.tar influxdb
+  echo "save compelted"
+}
+
+loadImage()
+{
+  if [ -f /backup/images/influxdb/influxdb.tar ]; then
+    echo "start loading influx image"
+    docker load  -i /backup/images/influxdb/influxdb.tar 
+    echo "load completed"
+  else
+    echo "backup image not found /backup/images/influxdb/influxdb.tar"
+  fi
+}
 
 while [[ $# -gt 0 ]]; do
     key="$1"
     case "$key" in
         # 
         run)
-        runDocker
+          runDocker
         ;;
         # 
         stop)
-        stopDocker
+          stopDocker
         ;;
         # 
         start)
-        startDocker
+          startDocker
         ;;
         # 
         remove)
-        removeDocker
+          removeDocker
 	;;
-	restore)
-	restoreDB
+	restoredb)
+	  restoreDB
+	;;
+        backupdb)
+          backupDB
+        ;;
+	save)
+	  saveImage
+	;;
+	load)
+	  loadImage
 	;;
         setup)
-        setupFolder
-	removeDocker
-	pullDocker
-	runDocker
-	setupDocker
+          setupFolder
+	  removeDocker
+	  pullDocker
+	  runDocker
+	  setupDocker
         ;;
         *)
         # Do whatever you want with extra options
         echo "Unknown option '$key'"
-	echo "possible options are: run stop start stop remove setup"
+	echo "possible options are: run stop start stop save load restoredb backupdb remove setup"
         ;;
     esac
     # Shift after checking all the cases to get the next option

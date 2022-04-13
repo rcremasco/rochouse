ef434d60b0627e8d491df8b352360386028dbb88
rcremasco <roberto.cremasco@gmail.com>
2022-04-13 16:12:51 +0200

    Update exec-httpdweewx.sh
    fix

--- scripts/exec-httpdweewx.sh
+++ scripts/exec-httpdweewx.sh
@@ -7,7 +7,8 @@ DOCKERNAME="httpd-weewx"
 DOCKERIMAGE="httpd"
 DOCKERVERSION="2.4"
 
-HTTPDW_ROOT=$APP/$DOCKERNAME
+HTTPDWEEWX_ROOT=$APP/$DOCKERNAME
+HTTPDWEEWX_CONF=$HTTPDWEEWX_ROOT/conf
 
 SCRIPTPATH=$(dirname $0)
 source $SCRIPTPATH/exec-common.sh
@@ -15,14 +16,20 @@ source $SCRIPTPATH/exec-common.sh
 setupFolder()
 {
   # Creazione directory
-  if [ ! -d  $WEEWX_ROOT ]; then
-    sudo mkdir -p $WEEWX_ROOT
-    writeLog "$WEEWX_ROOT created"
+  if [ ! -d  $HTTPDWEEWX_ROOT ]; then
+    sudo mkdir -p $HTTPDWEEWX_ROOT
+    writeLog "$HTTPDWEEWX_ROOT created"
   else
-    writeLog "$WEEWX_ROOT already present"
+    writeLog "$HTTPDWEEWX_ROOT already present"
   fi
 
-#  sudo chown -R 421:421 $WEEWX_ROOT
+  if [ ! -d  $HTTPDWEEWX_CONF ]; then
+    sudo mkdir -p $HTTPDWEEWX_CONF
+    writeLog "$HTTPDWEEWX_CONF created"
+  else
+    writeLog "$HTTPDWEEWX_CONF already present"
+  fi
+#  sudo chown -R 421:421 $HTTPDWEEWX_ROOT
 
 }
 
@@ -35,6 +42,7 @@ runDocker()
     writeLog "run $DOCKERNAME docker"
     docker run -d --name=$DOCKERNAME \
         -v /docker/weewx/html:/usr/local/apache2/htdocs \
+        -v $HTTPDWEEWX_CONF:/usr/local/apache2/conf \
         -p $VIP:8666:80 \
         --restart=always \
         $DOCKERIMAGE:$DOCKERVERSION

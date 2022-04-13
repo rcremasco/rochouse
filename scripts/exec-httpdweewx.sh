5d9c22d7379dce8b4172761a9895b206cd0f50b6
rcremasco <roberto.cremasco@gmail.com>
2022-04-13 10:22:49 +0200

    Add httpd

---
+++ scripts/exec-httpdweewx.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+
+APP="/docker"
+
+DOCKERNAME="httpdweewx"
+DOCKERIMAGE="tecmint-web"
+DOCKERVERSION="latest"
+
+HTTPDW_ROOT=$APP/$DOCKERNAME
+
+SCRIPTPATH=$(dirname $0)
+source $SCRIPTPATH/exec-common.sh
+
+setupFolder()
+{
+  # Creazione directory
+  if [ ! -d  $WEEWX_ROOT ]; then
+    sudo mkdir -p $WEEWX_ROOT
+    writeLog "$WEEWX_ROOT created"
+  else
+    writeLog "$WEEWX_ROOT already present"
+  fi
+
+#  sudo chown -R 421:421 $WEEWX_ROOT
+
+}
+
+
+
+runDocker()
+{
+  if ! isRunned ; then
+    writeLog "run $DOCKERNAME docker"
+    docker run -d --name=$DOCKERNAME \
+        -v /docker/weewx/html:/usr/local/apache2/htdocs \
+        -p 8666:8666 \        
+        --restart=always \
+        $DOCKERIMAGE:$DOCKERVERSION
+
+    writeLog "runed"
+
+    setupDocker
+  else
+    writeLog "$DOCKERNAME already running"
+  fi
+
+}
+
+setupDocker()
+{
+  writeLog "setting docker"
+  docker update  --restart=unless-stopped $DOCKERNAME
+}
+
+
+main "$@"
+
+exit

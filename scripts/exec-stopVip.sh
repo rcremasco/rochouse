c2c58c812310da84fd315f87f843881d7eec6574
rcremasco <roberto.cremasco@gmail.com>
2023-05-21 15:01:55 +0200

    Update exec-stopVip.sh

--- scripts/exec-stopVip.sh
+++ scripts/exec-stopVip.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 
 SCRIPTPATH=$(dirname $0)
 source $SCRIPTPATH/exec-common.sh
 
 
 #sudo ip address del $VIP/24 dev wlan0
-sudo ip address del $VIP/24 dev eth0
+#sudo ip address del $VIP/24 dev eth0
+INT="$(ip r | grep default | awk '{print $5}')"
+writeLog "Disattivazione IP $VIP da interfaccia $INT"
+
+sudo ip address del $VIP/24 dev $INT
